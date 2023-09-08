f3de66ab4e4a64ff2f4d4639fd8c89022c4b12d0
Jhonattan Lino <jhonattanlino@outlook.com>
2023-09-08 12:47:00 -0300

    Linear model crostruction

--- main.R
+++ main.R
@@ -5,7 +5,7 @@ if (!require(ISLR)) {
   install.packages('ISLR')
   library(ISLR)
 }
-
+library(ggplot2)
 #### Data ####
 data("Carseats") # Load dataset
 help('Carseats') # Doc
@@ -15,3 +15,11 @@ A variavél dependente será a variaver Sales e todas as outras variaveis serão
 '''
 dim(Carseats)
 summary(Carseats)
+
+#### Plotting data #####
+ggplot(Carseats, aes(x=Price, y=Sales))+
+  geom_smooth(method='loess')+
+  geom_point(size=2, col='blue')
+
+#### Linear Model ####
+  
